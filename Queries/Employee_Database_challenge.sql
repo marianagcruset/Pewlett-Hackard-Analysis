6117b2cd10317cff0e064606bcf8caa5eee8edc7
marianagcruset <marianagcruset@gmail.com>
2020-08-28 16:58:44 -0400

    Last changes

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,53 @@
+--Creation of retirement titles table
+SELECT e.emp_no,
+    e.first_name,
+	e.last_name,
+    t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) r.emp_no,
+	r.first_name,
+	r.last_name,
+	r.title
+INTO unique_titles
+FROM retirement_titles as r
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Employee count by title and create a new table to store it 
+SELECT COUNT(u.emp_no), 
+	u.title
+INTO retiring_titles
+FROM unique_titles as u
+GROUP BY u.title
+ORDER BY count(u.emp_no) DESC;
+-- Check the table
+SELECT * FROM retiring_titles;
+
+--Create the mentorship table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	 AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+
+
